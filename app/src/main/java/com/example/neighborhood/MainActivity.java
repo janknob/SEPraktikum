55e8e0856bbddeb05fb8a444683e174bd6c5c8d5
robinenders <robin.enders@hof-university.de>
2020-07-30 14:17:43 +0200

    man kann jetzt von der startseite auf die registerseite kommen

--- app/src/main/java/com/example/neighborhood/MainActivity.java
+++ app/src/main/java/com/example/neighborhood/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.neighborhood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button register, login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button register = (Button) findViewById(R.id.register);
+        Button login = (Button) findViewById(R.id.login);
+        register.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                toRegister();
+
+            }
+        });
+    }
+
+    private void toRegister() {
+        Intent intent = new Intent(this, RegisterActivity.class);
+
+        startActivity(intent);
+
+    }
+
+    private void toLogin() {
+
     }
 }
