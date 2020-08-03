5373021521bf2d9e2c50e6b7f255bdde3793fead
robinenders <robin.enders@hof-university.de>
2020-08-03 15:32:28 +0200

    firebase und friendlistactivität hinzugefügt

--- app/src/main/java/com/example/neighborhood/MainActivity.java
+++ app/src/main/java/com/example/neighborhood/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     protected void onCreate(Bundle savedInstanceState) {
-      //  super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button register = (Button) findViewById(R.id.register);
         Button login = (Button) findViewById(R.id.login);
