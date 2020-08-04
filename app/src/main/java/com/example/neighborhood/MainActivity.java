5e13870021a33bdf17871be242bb14940afbc118
PaulMerrick <paulzx4444@yahoo.de>
2020-08-04 11:17:58 +0200

    SplashScreen finished und reihenfolge festgelegt in der MainActivity

--- app/src/main/java/com/example/neighborhood/MainActivity.java
+++ app/src/main/java/com/example/neighborhood/MainActivity.java
@@ -5,26 +5,41 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button register, login;
+    Animation splash_anim;
+    ImageView logo;
     private static int SPLASH_SCREEN = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //Animations
+        splash_anim = AnimationUtils.loadAnimation(this, R.anim.splash_screen);
+
+        //Hooks
+        logo = findViewById(R.id.logo_iv);
+
+        logo.setAnimation(splash_anim);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent (MainActivity.this,RegisterActivity.class);
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
                 startActivity(intent);
                 finish();
             }
         }, SPLASH_SCREEN);
 
-        super.onCreate(savedInstanceState);
+        /*super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button register = (Button) findViewById(R.id.register);
         Button login = (Button) findViewById(R.id.login);
@@ -37,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
                 toRegister();
 
             }
-        });
+        });*/
     }
 
     /*new Handler().postDelayed(new Runnable()
--- app/src/main/java/com/example/neighborhood/SplashScreen.java
+++ app/src/main/java/com/example/neighborhood/SplashScreen.java
@@ -12,17 +12,8 @@ public class SplashScreen extends AppCompatActivity {
     Animation splash_anim;
     ImageView logo;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+    //@Override
+    /*protected void onCreate(Bundle savedInstanceState) {
 
-        //Animations
-        splash_anim = AnimationUtils.loadAnimation(this, R.anim.splash_screen);
-
-        //Hooks
-        logo = findViewById(R.id.logo_iv);
-
-        logo.setAnimation(splash_anim);
-    }
+    }*/
 }
