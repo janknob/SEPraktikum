1e39b17d3be6150b73c028f0416056a63aae5e2d
renders <robin.enders@hof-university.de>
2020-08-05 13:56:49 +0200

    visuell fragments

--- app/src/main/java/com/example/neighborhood/Fragment/ChatFragment.java
+++ app/src/main/java/com/example/neighborhood/Fragment/ChatFragment.java
@@ -7,15 +7,27 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.neighborhood.R;
 
 public class ChatFragment extends Fragment {
 
+    View view;
+    ListView listview;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        view = inflater.inflate(R.layout.fragment_chat, container, false);
+
+        listview = (ListView) view.findViewById(R.id.listview);
+        /*ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, freunde);
+        listview.setAdapter(adapter);*/
+
+        return view;
     }
 }
